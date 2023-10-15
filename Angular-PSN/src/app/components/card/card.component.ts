e86344f26a79052059b937031f0b50fe829eac70
MurilodioPy <engmurilo@edu.uniube.br>
2023-10-15 05:33:41 -0300

    ps-angular

--- Angular-PSN/src/app/components/card/card.component.ts
+++ Angular-PSN/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gameCover:String=""
+  @Input()
+  gameType:String="Digital PS4"
+  @Input()
+  gamePrice:String="R$ 399,90"
+  @Input()
+  gameLabel:String=""
   constructor() { }
 
   ngOnInit(): void {
